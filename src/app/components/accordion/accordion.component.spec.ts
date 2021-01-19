b0a713be8fb0f4e794ed4d0e8444dd80df191560
aidera <hope.aidera@gmail.com>
2021-01-19 14:14:21 +0200

    Added tests for accordion component

--- src/app/components/accordion/accordion.component.spec.ts
+++ src/app/components/accordion/accordion.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 import { AccordionComponent } from './accordion.component';
 
@@ -9,6 +12,11 @@ describe('AccordionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AccordionComponent],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientModule,
+        InlineSVGModule.forRoot(),
+      ],
     }).compileComponents();
   }));
 
@@ -20,5 +28,43 @@ describe('AccordionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle isOpen parameter', () => {
+    component.isOpen = true;
+    component.toggleIsOpen();
+
+    expect(component.isOpen).toBe(false);
+
+    component.isOpen = false;
+    component.toggleIsOpen();
+
+    expect(component.isOpen).toBe(true);
+  });
+
+  it('should not touch initialIsOpen parameter when toggle', () => {
+    component.isOpenInitial = true;
+    component.isOpen = true;
+
+    component.toggleIsOpen();
+
+    expect(component.isOpenInitial).toBe(true);
+
+    component.isOpen = false;
+    component.toggleIsOpen();
+
+    expect(component.isOpenInitial).toBe(true);
+
+    component.isOpenInitial = false;
+    component.isOpen = true;
+
+    component.toggleIsOpen();
+
+    expect(component.isOpenInitial).toBe(false);
+
+    component.isOpen = false;
+    component.toggleIsOpen();
+
+    expect(component.isOpenInitial).toBe(false);
   });
 });
