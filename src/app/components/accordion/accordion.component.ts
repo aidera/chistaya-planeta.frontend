3c382da4ff4e5132b18f46bab1590fbf52e53ed2
aidera <hope.aidera@gmail.com>
2021-02-11 10:40:27 +0200

    Fixed accordion's height

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,18 +1,11 @@
-import {
-  Component,
-  Input,
-  ViewChild,
-  ElementRef,
-  AfterViewInit,
-  OnInit,
-} from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss'],
 })
-export class AccordionComponent implements OnInit, AfterViewInit {
+export class AccordionComponent implements OnInit {
   @Input() title: string;
   @Input() isOpenInitial?: boolean;
   @ViewChild('accordionContent') accordionContentRef: ElementRef;
@@ -22,22 +15,10 @@ export class AccordionComponent implements OnInit, AfterViewInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.isOpen = !!this.isOpenInitial;
-  }
-
-  ngAfterViewInit(): void {
-    this.maxAccordionContentHeight = this.accordionContentRef.nativeElement.offsetHeight;
-    this.setContentMaxHeight();
+    this.isOpen = this.isOpenInitial;
   }
 
   public toggleIsOpen(): void {
     this.isOpen = !this.isOpen;
-    this.setContentMaxHeight();
-  }
-
-  private setContentMaxHeight(): void {
-    this.accordionContentRef.nativeElement.style.maxHeight = this.isOpen
-      ? this.maxAccordionContentHeight + 'px'
-      : '0';
   }
 }
