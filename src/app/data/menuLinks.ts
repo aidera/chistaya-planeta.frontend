0283f944a6617adba79422961c38b9b2d94addaa
aidera <hope.aidera@gmail.com>
2021-02-25 18:58:31 +0200

    Temporary hide of unused links

--- src/app/data/menuLinks.ts
+++ src/app/data/menuLinks.ts
@@ -6,16 +6,16 @@ export const menuLinksMain: MenuLink[] = [
     iconPath: 'assets/icons/order.svg',
     link: 'orders',
   },
-  {
-    title: 'Периодические заявки',
-    iconPath: 'assets/icons/scheduled-order.svg',
-    link: 'scheduled-orders',
-  },
-  {
-    title: 'Клиенты',
-    iconPath: 'assets/icons/customer.svg',
-    link: 'clients',
-  },
+  // {
+  //   title: 'Периодические заявки',
+  //   iconPath: 'assets/icons/scheduled-order.svg',
+  //   link: 'scheduled-orders',
+  // },
+  // {
+  //   title: 'Клиенты',
+  //   iconPath: 'assets/icons/customer.svg',
+  //   link: 'clients',
+  // },
   {
     title: 'Сотрудники',
     iconPath: 'assets/icons/tie.svg',
